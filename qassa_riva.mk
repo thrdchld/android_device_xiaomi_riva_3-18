f9812a9e9941b1e690f7785de4926f21c2d9ff81
thrd chld <thirdchilddesigner@gmail.com>
2024-07-02 17:14:11 +0000

    adapt to qassa

--- qassa_riva.mk
+++ qassa_riva.mk
@@ -20,17 +20,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common qassa stuff
+$(call inherit-product, vendor/qassa/config/common_full_phone.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# QASSA UNOFFICIAL stuff
+QASSA_MAINTAINER := thrdchld
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+USE_PIXEL_CHARGER := true
+TARGET_APERTURE_OPTOUT := true
+QTI_OPTOUT := true
+TARGET_MIXPLORER_OPTOUT := true
+
+# Google Connectivity Services
+TARGET_INCLUDE_WIFI_EXT := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := qassa_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
